95d80d2c04ed15a9b5cfb7b22fd34e314559f885
getmemice <jiwon.kim4@gmail.com>
2021-10-13 19:12:45 +0900

    first commit

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/carActions'
+
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+  console.log(cartItems)
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'></ListGroup>
+        )}
+      </Col>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+      </Col>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+      </Col>
+    </Row>
+  )
+}
+
+export default CartScreen
